2a84b293e90a9b73d40f6cc68ee11ade49117d94
valmir <valmirchagas88@gmail.com>
2023-02-15 00:04:12 -0300

    chore: add nodemon lib

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 
 module.exports = app;
