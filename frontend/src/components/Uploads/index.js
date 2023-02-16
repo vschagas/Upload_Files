90d0554a13da526046241ade61ac8b2f3f3ad7ca
user <valmirchagas88@gmail.com>
2023-02-16 10:23:23 -0300

    chore: add React drop-zone lib

--- frontend/src/components/Uploads/index.js
+++ frontend/src/components/Uploads/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
 
 export default class Upload extends Component {
   render() {
     return (
-      <div>index</div>
+      <Dropzone></Dropzone>
     )
   }
 }
