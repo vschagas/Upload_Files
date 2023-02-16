aa176064f28b87939a3b35a5c4124a30885532c2
user <valmirchagas88@gmail.com>
2023-02-16 15:51:58 -0300

    feat: add Dropzone component

--- frontend/src/components/Uploads/index.js
+++ frontend/src/components/Uploads/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
 
+import { DropContainer, UPloadMessage } from './styles'
+
 export default class Upload extends Component {
   render() {
     return (
-      <Dropzone></Dropzone>
+      <Dropzone accept="image/*" onDropAccepted={ () => {} }>
+        { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer
+          { ...getRootProps() }
+          isDragActive={isDragActive}
+          isDragReject={isDragReject}
+          >
+            <input { ...getInputProps }/>
+          </DropContainer>
+        )}
+        
+      </Dropzone>
     )
   }
 }
