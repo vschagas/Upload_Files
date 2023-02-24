f782d0c17d545e6eca4a63d8cdac222575a77584
user <valmirchagas88@gmail.com>
2023-02-24 11:19:18 -0300

    feat: add handleUpload function to set state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,39 @@ import GlobalStyle from './styles/global'
 import { Container, Content } from './styles'
 import Upload from './components/Uploads'
 import FileList from './components/files/'
+import { uniqueId } from 'lodash'
+import { filesize } from 'filesize'
 
 export class App extends Component {
+  state = {
+    uploadedFiles: []
+  }
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null,
+    }))
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    })
+  };
+
   render() {
+    const { uploadedFiles } = this.state
     return (
       <Container>
         <Content>
-          <Upload />
-          <FileList />
+          <Upload onUpload={ this.handleUpload } />
+          { !!uploadedFiles.length && <FileList files={ uploadedFiles } /> }
         </Content>
         <GlobalStyle />
       </Container>
