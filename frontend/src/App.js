e071e2fe32c7fc49e89c365894cdb2bfa3d3ebaf
user <valmirchagas88@gmail.com>
2023-03-08 10:04:25 -0300

    feat: add update files and api post feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,13 +29,29 @@ export class App extends Component {
       uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
     })
 
-    uploadedFiles.forEach(this.proccessUpload);
-
-    proccessUpload = (uploadedFile) => {
-
-    }
-
+    uploadedFiles.forEach(this.processUpload);
   };
+
+  updateFile = ( id, data) => {
+    this.setState({ uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+      return id === uploadedFile.id ? { ...uploadedFile, ...data } : uploadedFile;
+    })})
+  }
+
+  processUpload = (uploadedFile) => {
+    const data = new FormData();
+    data.append('file', uploadedFile.file, uploadedFile.name );
+
+    api.post('/posts', data, {
+      onUploadProgress: e => {
+        const progress = parseInt(Math.round((e.loaded * 100) / e.total))
+
+        this.updateFile(uploadedFile.id, {
+          progress,
+        })
+      }
+    })
+  }
 
   render() {
     const { uploadedFiles } = this.state
